dc0cb19d672f2e5e67b4c577c2ed9b02d4ade565
Arif <khadraniarif@gmail.com>
2018-11-27 23:31:39 +0530

    Initial code

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ChannelsField from './ChannelsField'
+import Button from '../containers/Button'
+import TopNews from '../containers/TopNews'
+
+const MainContainer = () => (
+  <div>
+    < ChannelsField />
+    <Button />
+    <TopNews />
+  </div>
+)
+
+export default MainContainer;
